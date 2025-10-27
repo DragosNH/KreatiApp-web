3609dcac4f59bb6bc501d1560d96caea0c5f2fe6
Dragoş <edragos33@gmail.com>
2025-10-27 09:48:05 +0100

    login form added

--- KreatiApp/users/views.py
+++ KreatiApp/users/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 def users(request):
     return render(request, 'index.html')
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
