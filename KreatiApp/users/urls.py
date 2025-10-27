998c32922c78e88b315affdac34853cb624a78c0
Dragoş <edragos33@gmail.com>
2025-10-27 10:15:24 +0100

    added sign up page and forgot password

--- KreatiApp/users/urls.py
+++ KreatiApp/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import SignUpView
 
 
 urlpatterns = [
     path('', views.users, name='users'),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
